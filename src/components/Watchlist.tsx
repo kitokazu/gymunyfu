2b7a7fc0babb328c78b1854e9e3e679d85fc5fce
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2024-11-05 22:18:20 +0900

    invesment watchlist placeholder

--- src/components/Watchlist.tsx
+++ src/components/Watchlist.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Watchlist() {
   return (
     <div className="">
-      <div className="hidden h-full w-[250px] transform border-r bg-accent transition-all duration-300 ease-in-out sm:flex">
+      <div className="hidden h-full w-[300px] transform border-r bg-accent transition-all duration-300 ease-in-out sm:flex">
         <aside className="flex h-full w-full columns-1 flex-col overflow-x-hidden break-words px-4">
           {/* Top */}
           <div className="relative mt-4 pb-2">
@@ -15,7 +15,7 @@ export default function Watchlist() {
             <div className="flex flex-col items-center justify-center gap-4 align-middle text-lg">
               <div className="text-neutral-500 dark:text-neutral-400"></div>
             </div>
-            ≤<div className="mt-8 flex flex-col space-y-4"></div>
+            <div className="mt-8 flex flex-col space-y-4"></div>
           </div>
           {/* Bottom */}
           <div className="sticky bottom-0 mb-4 mt-auto block whitespace-nowrap transition duration-200"></div>
