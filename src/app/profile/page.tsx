d2b6f14aec97564cf2aadfdd5706ab526359bfd7
Kai Itokazu <56910039+kitokazu@users.noreply.github.com>
2024-11-05 23:25:35 +0900

    missed file

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,11 +6,11 @@ export default function Profile() {
       <header>
         <h1 className="text-3xl font-bold">Profile</h1>
       </header>
-
       <main className="w-full">
         <div className="px-6 lg:px-48"></div>
         <hr className="my-4 w-full border-gray-300" />
       </main>
+      s
     </section>
   );
 }
